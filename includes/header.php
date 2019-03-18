15b35b039a0f18ac00314e013fb5d19607782d6e
Aaron_At_Joffrey <aglynn@joffrey.org>
2019-03-18 16:10:28 -0500

    restyled, labeled nav, refactored into header and footer

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name=viewport content="width=device-width, initial-scale=1">
+<title>Joffrey Ballet - SWAC</title>
+<link rel="stylesheet" href="resetstyle.css">
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<nav class="nav-main">
+	<div class="btn-toggle-nav" onclick="toggleNav()"></div>
+	<ul>
+		<li><a href="#">calendar</a></li>
+	</ul>
+</nav>
+
+<aside class="nav-sidebar"> 
+	<ul>
+		<li><span>admin create menu:</span></li>
+		<li><a href="#">performances</a></li>
+		<li><a href="#">rehearsals</a></li>
+		<li><a href="#">roles</a></li>
+		<li><a href="#">role conflicts</a></li>
+		<li><a href="#">dancers</a></li>
+		<li><a href="#">notification templates</a></li>
+		
+	</ul>
+</aside>
+	
+	
