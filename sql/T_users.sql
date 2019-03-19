24e967cb5e4acf11013d8fe3b4cb82dacc8dfb08
Aaron_At_Joffrey <aglynn@joffrey.org>
2019-03-19 15:19:43 -0500

    prevented directory listing

---
+++ sql/T_users.sql
@@ -0,0 +1,14 @@
+create table users(
+user_uid int AUTO_INCREMENT primary key,
+user_first text not null,
+user_last text not null,
+user_email text not null,
+user_pwd text not null,
+can_create int DEFAULT 0
+);
+
+insert into users(user_first, user_last, user_email, user_pwd, can_create)
+values('Aaron', 'Glynn', 'aglynn@joffrey.org', '123', 1);
+
+insert into users(user_first, user_last, user_email, user_pwd, can_create)
+values('Paul', 'Key', 'pkey@joffrey.org', '123', 0);
