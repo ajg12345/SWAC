90c56c5ea065a4ff9c314fe2bc9c2890c79d6238
Aaron_At_Joffrey <aglynn@joffrey.org>
2019-04-24 11:13:21 -0500

    updating the user table to match the ER diagram

--- sql/T_users.sql
+++ sql/T_users.sql
@@ -1,7 +1,6 @@
 create table users(
 user_uid int AUTO_INCREMENT primary key,
-user_first text not null,
-user_last text not null,
+user_fullname text not null,
 user_email text not null,
 user_pwd text not null,
 can_create int DEFAULT 0
