e0bdff81b1ce5340ab57a83e591836506c22571a
Aaron_At_Joffrey <aglynn@joffrey.org>
2019-04-24 13:21:04 -0500

    fix user table SQL example statment, adding ER diagram

--- sql/T_users.sql
+++ sql/T_users.sql
@@ -6,8 +6,8 @@ user_pwd text not null,
 can_create int DEFAULT 0
 );
 
-insert into users(user_first, user_last, user_email, user_pwd, can_create)
-values('Aaron', 'Glynn', 'aglynn@joffrey.org', '123', 1);
+insert into users(user_fullname, user_email, user_pwd, can_create)
+values('Aaron Glynn', 'aglynn@joffrey.org', '123', 1);
 
-insert into users(user_first, user_last, user_email, user_pwd, can_create)
-values('Paul', 'Key', 'pkey@joffrey.org', '123', 0);
+insert into users(user_fullname, user_email, user_pwd, can_create)
+values('Paul Key', 'pkey@joffrey.org', '123', 0);
