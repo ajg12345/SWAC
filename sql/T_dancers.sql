9a366dc92fdf27db46fa7c88e9245afd995ce885
Aaron_At_Joffrey <aglynn@joffrey.org>
2019-04-24 11:09:12 -0500

    updating the dancer table to match the ER diagram

--- sql/T_dancers.sql
+++ sql/T_dancers.sql
@@ -1,10 +1,10 @@
 create table dancers(
-id int AUTO_INCREMENT primary key,
-dancer_first text not null,
-dancer_last text not null,
-dancer_email text not null
+dancer_id int AUTO_INCREMENT primary key,
+dancer_fullname text not null,
+dancer_email text not null,
+dancer_phone text not null
 );
 
-insert into dancers(dancer_first, dancer_last, dancer_email)
-values('Fabrice', 'Calmels', 'fcalmels@joffrey.org');
+insert into dancers(dancer_fullname, dancer_phone, dancer_email)
+values('Fabrice Calmels', '(123) 123-4567', 'fcalmels@joffrey.org');
 
