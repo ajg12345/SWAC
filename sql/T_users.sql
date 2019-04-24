b336eeae1bdb513a1dc26d1d2674e08287e44c0e
Aaron_At_Joffrey <aglynn@joffrey.org>
2019-04-24 13:26:12 -0500

    fix user table again due to signup sheet requirements

--- sql/T_users.sql
+++ sql/T_users.sql
@@ -1,13 +1,8 @@
 create table users(
 user_uid int AUTO_INCREMENT primary key,
-user_fullname text not null,
+user_first text not null,
+user_last text not null,
 user_email text not null,
 user_pwd text not null,
 can_create int DEFAULT 0
 );
-
-insert into users(user_fullname, user_email, user_pwd, can_create)
-values('Aaron Glynn', 'aglynn@joffrey.org', '123', 1);
-
-insert into users(user_fullname, user_email, user_pwd, can_create)
-values('Paul Key', 'pkey@joffrey.org', '123', 0);
