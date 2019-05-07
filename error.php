a0a1a3cdbbbdcbf0153f73d553018a171abf02b7
Aaron_At_Joffrey <aglynn@joffrey.org>
2019-05-07 11:36:11 -0500

    created role delete. error check for role_conflicts and castings references before delete from roles.

--- error.php
+++ error.php
@@ -1,12 +1,30 @@
 <?php
 include "includes/header.php";
+$error_message = "";
+$error_code = trim($_GET["error_code"]);
+if (!empty($error_code)){
+	if(strcmp($error_code,1) == 0){
+		$error_message = "The role cannot be deleted because it is referenced in a casting and/or role conflict.<br>Delete the casting and/or role conflict record first.";
+	}elseif(strcmp($error_code,2) == 0){
+		$error_message = "A role was not deleted because no role_id was specified in the call to the delete page.";
+	}else{
+		$error_message = "Unhandled error code in url. Please consult the code master.";
+	}
+}
 ?>
 <div class="grid">
 	<div class="title">
 		<h1>Invalid Request</h1>
 	</div>
 	<div class="content">
-		<p>Sorry, you've made an invalid request. Please <a href="index.php" class="alert-link">go back</a> and try again.</p>
+		<?php 
+		if(!empty($error_message)){
+			echo "<p>".$error_message."</p>";
+		} else{
+			echo '<p>Sorry, you have made an invalid request. Please <a href="index.php" class="alert-link">go back</a> and try again.</p>';
+		}
+			
+		?>
 	</div>
 </div>
 <?php 
---
+++ roledelete.php
@@ -0,0 +1,87 @@
+<?php
+require_once "includes/header.php";
+require_once "includes/dbh.inc.php";
+
+$conflict_role_id = '';
+$casting_role_id = '';
+
+// Process delete operation after confirmation
+if(isset($_POST["role_id"]) && !empty($_POST["role_id"])){    
+    // Prepare a delete statement
+	$param_id = trim($_POST["role_id"]);
+	$conflict_check = "select role_id1 from role_conflicts where role_id1=".$param_id." limit 1;";
+	$casting_check = "select role_id from castings where role_id=".$param_id." limit 1;";
+	
+	$conflict_result = mysqli_query($conn,$conflict_check);
+	$conflict_row = mysqli_fetch_array($conflict_result);
+	$conflict_role_id = $conflict_row['role_id1'];
+	mysqli_free_result($conflict_row);
+	
+	$casting_result = mysqli_query($conn,$casting_check);
+	$casting_row = mysqli_fetch_array($casting_result);
+	$casting_role_id = $casting_row['role_id'];
+	mysqli_free_result($cassting_row);
+	
+    $sql = "DELETE FROM roles WHERE role_id = ?";
+    
+    if($stmt = mysqli_prepare($conn, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+        // Set parameters
+        
+		//check that this role is not referenced in role_conflicts or castings
+        if (empty($casting_role_id) && empty($conflict_role_id)){
+			// Attempt to execute the prepared statement
+			if(mysqli_stmt_execute($stmt)){
+				// Records deleted successfully. Redirect to landing page
+				header("location: roles.php");
+				exit();
+			} else{
+				echo "Oops! Something went wrong. Please try again later.";
+			}
+		} else {
+			$error_code = 1;
+			$header_target = "location: error.php?error_code=".$error_code;
+			header($header_target);
+			exit();
+		}
+    }
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($conn);
+} else{
+    // Check existence of id parameter
+    if(empty(trim($_GET["role_id"]))){
+        // URL doesn't contain id parameter. Redirect to error page
+		$error_code = 2;
+		$header_target = "location: error.php?error_code=".$error_code;
+        header($header_target);
+        exit();
+    }
+}
+include_once "includes/crudheader.php";
+?>
+ <div class="grid">
+	<div class="title">
+		<h1>Delete Role</h1>
+	</div>
+	<div class="content">
+	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+		<div class="alert alert-danger fade in">
+			<input type="hidden" name="role_id" value="<?php echo trim($_GET["role_id"]); ?>"/>
+			<p>Are you sure you want to delete this role?</p><br>
+			<p>
+				<input type="submit" value="Yes" class="btn btn-danger">
+				<a href="roles.php" class="btn btn-default">No</a>
+			</p>
+		</div>
+	</form>
+	</div>
+</div>        
+<?php
+include_once "includes/footer.php";
+?>
