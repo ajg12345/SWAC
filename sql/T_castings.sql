dfabf0a0f23e4f09b7f8769e88b4c500b08932b1
Aaron_At_Joffrey <aglynn@joffrey.org>
2019-05-16 16:28:14 -0500

    updated casting table to include date, (so that updates would appear highlighted in client screen) and ER-Diagram.

--- sql/T_castings.sql
+++ sql/T_castings.sql
@@ -2,14 +2,15 @@ create table castings(
 casting_id int AUTO_INCREMENT primary key,
 re_id int,
 role_id int,
-dancer_id int
+dancer_id int,
+update_dt datetime
 );
 
 
-insert into castings(re_id, role_id, dancer_id)
-values(1,1,1);
+insert into castings(re_id, role_id, dancer_id, update_dt)
+values(1,1,1, NOW());
 
-insert into castings(re_id, role_id, dancer_id)
-values(1,2,2);
+insert into castings(re_id, role_id, dancer_id, update_dt)
+values(1,2,2, NOW());
 
 
